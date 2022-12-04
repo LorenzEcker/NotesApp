0c74049aff470490c3495ab9c4bc8d3011874b57
Lorenz Ecker <lorecker1@gmail.com>
2022-12-04 12:19:47 +0100

    initial commit

---
+++ NotesApp.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NotesApp.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ NotesApp.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NotesApp.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
